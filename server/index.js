d9a9ccecd8e91b0e5092a4c10df582ae6c298462
codeAkhilesh14 <av384783@gmail.com>
2026-02-21 16:16:03 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 8000
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://websitebuilder-mw6x.onrender.com",
     credentials: true
 }))
 
